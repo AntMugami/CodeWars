5d20612c64af1105167cdd4ee49e7404f88ab88b
Anton Mugami <a.sheludyak@gmail.com>
2024-11-08 14:01:37 +0500

    BinCount

---
+++ BinNumofOnes.py
@@ -0,0 +1,13 @@
+def count_bits(n):
+    str_bin_num = str(bin(n))
+    return str_bin_num.count('1',2,)
+
+
+# def countBits(n):
+#     return bin(n).count("1")
+
+
+print(count_bits(4))
+print(count_bits(7))
+print(count_bits(9))
+print(count_bits(10))
